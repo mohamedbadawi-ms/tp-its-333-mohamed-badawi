a39124ce7935ac8517105b139b1c9bedb7dad87d
Mohamed Badawi <mohamed.badawi0524@gmail.com>
2026-01-12 16:36:23 +0100

    Premier commit

--- srcCodeAPIs/myFirstAPI_with_webAPI/app.py
+++ srcCodeAPIs/myFirstAPI_with_webAPI/app.py
@@ -3,8 +3,16 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 ## EXO1: API GET: renvoyer un helloworld - API end point name: "api/salutation"
+@app.route('/api/salutation', methods=['GET'])
+def salutation():
+    return jsonify({"message": "Hello, World!"})
 
 ## EXO2: API POST: renvoyer un nom fourni en parametre - API end point name: "api/utilisateurs"
+@app.route('/api/utilisateurs', methods=['POST'])
+def ajouter_utilisateur():
+    data = request.get_json()
+    nom = data.get('nom', 'Inconnu')
+    return jsonify({"message": f"Utilisateur {nom} ajouté avec succès!"})
 
 # to be tested with curl: 
 # >> curl -i -X GET http://localhost:5000/api/salutation
